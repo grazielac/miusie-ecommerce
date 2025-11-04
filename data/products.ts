9482ba059de27aa8a849afe94f5d32aebc6262d0
grazielac <cpgraziela@gmail.com>
2025-11-04 12:43:34 +0000

    Add £ sign for price

--- data/products.ts
+++ data/products.ts
@@ -7,8 +7,7 @@ interface ProductProps {
     description: string;
     price: string;
     image: StaticImageData;
-
-    
+    slug: string;
 }
 
 export const products: ProductProps[] = [
@@ -16,35 +15,40 @@ export const products: ProductProps[] = [
         id: 1,
         title: "Flower Mug",
         description: "Morning mug to start your day",
-        price: "40",
+        price: "£40",
         image: lovenote,
+        slug: "flower-mug"
     },
     {
         id: 2,
         title: "Bow Mug",
         description: "Morning mug to start your day",
-        price: "40",
+        price: "£40",
         image: lovenote,
+        slug: "bow-mug"
     },
     {
         id: 3,
         title: "Summer Mug",
         description: "Morning mug to start your day",
-        price: "40",
+        price: "£40",
         image: lovenote,
+        slug: "summer-mug"
     },
     {
         id: 4,
         title: "Autumn Mug",
         description: "Morning mug to start your day",
-        price: "40",
+        price: "£40",
         image: lovenote,
+        slug: "autumn-mug"
     },
     {
         id: 5,
         title: "Tulip Mug",
         description: "Morning mug to start your day",
-        price: "40",
+        price: "£40",
         image: lovenote,
+        slug: "tulip-mug"
     },
 ];
