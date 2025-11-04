5a0d70aade7cae9875632692930356a66dce4489
grazielac <cpgraziela@gmail.com>
2025-11-04 20:45:47 +0000

    Make product page responsive

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { products } from "@/data/products";
 import { useParams } from "next/navigation";
+import NavBar from "@/components/NavBar";
 
 function ProductPage() {
     const { slug } = useParams() as { slug: string };
@@ -15,31 +16,32 @@ function ProductPage() {
 
   return (
     <>
-      <div className="w-full min-h-screen bg-red-200 p-10">
-        <div className="w-full p-5 pb-6 bg-red-300">
-          <div className="flex">
-            <div>
+    <NavBar />
+      <div className="w-full bg-[#FBF7F6] p-10">
+        <div className="w-full">
+          <div className="sm:flex pt-8">
+            <div className="pt-6 sm:pl-10 ">
               <Image 
               src={product.image} 
               alt="title" 
-              width={400}
+              width={500}
               height={200}
-              className="rounded-lg" />
+              className="rounded-3xl sm:w-250" />
             </div>
 
-            <div className="bg-amber-300">
-              <div>
-                <h1 className="text-xl">{product.title}</h1>
-                <h3 className="text-sm font-semibold">{product.price}</h3>
+            <div className="w-full sm:pl-14">
+              <div className="pt-6 sm:pt-14">
+                <h1 className="text-2xl sm:text-3xl pb-2">{product.title}</h1>
+                <h2 className="text-xl sm:text-2xl font-semibold">{product.price}</h2>
               </div>
               <div>
-                <p className="text-sm">{product.description}</p>
+                <p className="py-10 text-sm">{product.description}</p>
               </div>
 
               {/* buttons */}
-              <div className="flex justify-around">
-                <button className="bg-amber-100"> + 1 </button>
-                <button className="bg-amber-200">Add to Cart</button>
+              <div className=" flex justify-center-safe gap-8 sm:justify-evenly pb-8 sm:mt-10 sm:w-3/5">
+                <button className="border-red-300 border py-2 px-14 rounded-full cursor-pointer">  + 1 </button>
+                <button className="bg-[#820C18] text-white py-2 px-14 rounded-full cursor-pointer">Add to Cart</button>
               </div>
             </div>
           </div>
