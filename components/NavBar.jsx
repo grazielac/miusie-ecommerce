13bef0723d6ee3e73d9d2dc042acf8e7aebac884
grazielac <cpgraziela@gmail.com>
2025-11-10 17:08:29 +0000

    Add links to navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 function NavBar() {
   return (
     <>
       <div className="text-sm flex gap-4 justify-around border-t-gray-400 border-b-gray-400 border-t border-b pt-8 pb-7">
         <div className="w-full flex justify-around">
-          <h2>Home</h2>
-          <h2>About</h2>
-          <h2>Shop</h2>
-          <h2>Cart</h2>
+          {/* <Link href="/">Home</Link> */}
+          {/* <Link href="/about">About</Link> */}
+          <Link href="/shop">Shop</Link>
+          <Link href="/cart">Cart</Link>
         </div>
 
       </div>
