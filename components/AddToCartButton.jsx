286cae53b8fa91ad5ebcda5db4a60c05b54d8eff
grazielac <cpgraziela@gmail.com>
2025-11-10 19:00:02 +0000

    Fix: typo file name - js to jsx

---
+++ components/AddToCartButton.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useCart } from "@/context/CartProvider";
+
+function AddToCartButton({ product }) {
+  const { cart, addToCart } = useCart();
+
+  console.log(cart);
+
+  return (
+    <div>
+      <div className=" flex justify-center-safe gap-8 sm:justify-evenly pb-8 sm:mt-10 sm:w-3/5">
+        <button className="border-red-300 border py-2 px-14 rounded-full cursor-pointer">
+          + 1
+        </button>
+        <button
+          onClick={() => addToCart(product)}
+          className="bg-[#820C18] text-white py-2 px-14 rounded-full cursor-pointer"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AddToCartButton;
