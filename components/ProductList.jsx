26c270c548fb0ea0f58c45ecb1979d89bd1b0094
grazielac <cpgraziela@gmail.com>
2025-11-10 16:36:43 +0000

    Remove props and destructure {product}

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { products } from "@/data/products";
 import ProductCard from "./ProductCard";
 
@@ -5,23 +6,17 @@ function ProductList() {
   return (
     <>
       <div className="w-full h-screen bg-amber-100">
-        <div>
-          <h2 className="text-xl">MUGS</h2>
-        </div>
-
         {/* all products */}
-        <div className=" bg-red-200 grid grid-cols-2">
-          <div className="w-100">
+        <div className=" bg-red-200 w-full p-6">
+          <h2 className="text-xl">MUGS</h2>
+
+          <div className="grid grid-cols-4 gap-6">
             {/* map through products */}
             {products.map((product) => (
-              <ProductCard
-                key={product.id}
-                image={product.image}
-                title={product.title}
-                description={product.description}
-                price={product.price}
-                slug={product.slug}
-              />
+              <div key={product.id} className="mb-8">
+                <ProductCard product={product} />
+                
+              </div>
             ))}
           </div>
         </div>
