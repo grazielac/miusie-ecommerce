8ad80aa5e201cf5cec3c85d7eb2e561bf3e813ab
grazielac <cpgraziela@gmail.com>
2025-11-10 20:13:54 +0000

    Create Cart route page

---
+++ app/cart/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CartDisplay from '../../components/CartDisplay';
+
+function Cart() {
+  return (
+    <div>
+      <CartDisplay />
+    </div>
+  )
+}
+
+export default Cart;
