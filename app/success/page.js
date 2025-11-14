cb6baa6e383840cc06d425e9eb6d79ded2d79d93
grazielac <cpgraziela@gmail.com>
2025-11-14 20:14:15 +0000

    Add success page

--- app/success/page.js
+++ app/success/page.js
@@ -1,10 +1,20 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import Link from "next/link";
- 
-function SuccessPage() {
-    const searchParams = useSearchParams();
-    const sessionId = searchParams.get("session_id");
+
+function Success() {
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    if (sessionId) {
+      fetch(`api/orders/by-session/${sessionId}`)
+        .then((res) => res.json())
+        .then((data) => setOrder(data));
+    }
+  }, [sessionId]);
 
   return (
     <div className="min-h-screen flex items-center justify-center">
@@ -13,16 +23,17 @@ function SuccessPage() {
         <h1>Payment Successful!</h1>
         <p>Thank you for your purchase. Your order has been confirmed.</p>
         {sessionId && (
-            <p className="text-sm text-gray-500 mb-6">
-                Order ID: {sessionId}
-            </p>
+          <p className="text-sm text-gray-500 mb-6">Order ID: {order?.id}</p>
         )}
-        <Link href="/" className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600">
-        Continue Shopping
+        <Link
+          href="/"
+          className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
+        >
+          Continue Shopping
         </Link>
       </div>
     </div>
   );
 }
 
-export default SuccessPage;
+export default Success;
