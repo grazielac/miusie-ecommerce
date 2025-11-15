02988bafc77e5f1030a2865efce86fc251a4a8b9
grazielac <cpgraziela@gmail.com>
2025-11-15 12:00:28 +0000

    Export 'force-dynamic' to render page dynamically instead of pre-render at build time

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import SuccessContent from "./SuccessContent";
 
+// tels next.js to render this page dynamically on each request instead of trying to pre-render it at build time
+export const dynamic = 'force-dynamic';
+
 function SuccessPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
