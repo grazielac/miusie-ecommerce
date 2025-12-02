a97b6641938552e868026f65f0063b5808c56d33
grazielac <cpgraziela@gmail.com>
2025-12-02 10:49:48 +0000

    Better button

--- components/AddToCartButton.jsx
+++ components/AddToCartButton.jsx
@@ -5,15 +5,15 @@ function AddToCartButton({ product }) {
   const { addToCart } = useCart();      
 
   return (
-    <div>
-      <div className=" flex justify-center-safe gap-8 sm:justify-evenly pb-8 sm:mt-10 sm:w-3/5">
+    <div className="flex justify-center items-center">
+      <div className="flex gap-8 sm:justify-evenly pb-8 sm:mt-10 sm:w-3/5">
       
-        <button className="border-red-300 border py-2 px-14 rounded-full cursor-pointer">
-          + 1
+        <button className="border-red-300 text-sm border py-2 px-12 rounded-full cursor-pointer">
+          -  1  +
         </button>
         <button
           onClick={() => addToCart(product)}
-          className="bg-[#820C18] text-white py-2 px-14 rounded-full cursor-pointer"
+          className="bg-[#820C18] text-white text-sm py-2 px-10 rounded-full cursor-pointer"
         >
           Add to Cart
         </button>
