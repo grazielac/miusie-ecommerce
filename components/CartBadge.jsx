a8a7c9ab25c059bb4b1931a9be20e1b10a8416d7
grazielac <cpgraziela@gmail.com>
2025-12-03 12:28:50 +0000

    Create cart badge seperately from navbar

---
+++ components/CartBadge.jsx
@@ -0,0 +1,16 @@
+// CartBadge.jsx
+"use client";
+import { useCart } from "@/context/CartProvider";
+
+export default function CartBadge() {
+  const { cart } = useCart();
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+  if (totalItems === 0) return null;
+
+  return (
+    <span className="absolute top-4 right-10 bg-red-800 text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
+      {totalItems}
+    </span>
+  );
+}
