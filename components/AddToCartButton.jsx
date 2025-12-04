6c9f85f44c45ec3012e2b20109180742d0973914
grazielac <cpgraziela@gmail.com>
2025-12-04 12:57:09 +0000

    fix: responsive layout button for mobile

--- components/AddToCartButton.jsx
+++ components/AddToCartButton.jsx
@@ -19,19 +19,23 @@ function AddToCartButton({ product, quantity }) {
   return (
     <div className="flex justify-center items-center">
       <div className="flex gap-8 sm:justify-evenly pb-8 sm:mt-10 sm:w-3/5">
-        <div className="flex flex-col sm:flex-row sm:gap-4 gap-2 items-center">
-          <div className="flex items-center justify-center w-16 text-sm py-2 px-8 border sm:px-12 lg:py-3 lg:px-16 rounded-full">
-            {/* MINUS */}
+        <div className="flex flex-col sm:flex-row sm:gap-4 lg:gap-18 gap-2 items-center">
+
+          {/* ADD/MINUS ITEM */}
+          <div className="flex items-center justify-center w-34 text-sm px-8 border sm:px-12 sm:py-2 lg:px-16 rounded-full">
+            {/* minues */}
             <button
-              onClick={() => minusItem(product, quantity)}
+              onClick={minusItem}
               className="p-2 hover:bg-red-100 rounded-full"
             >
               <HiOutlineMinus size={20} className="cursor-pointer" />
             </button>
-            <div>{onquantity}</div>
-            {/* PLUS */}
+
+            <div className="px-2">{onquantity}</div>
+
+            {/* add */}
             <button
-              onClick={() => addItem(product, quantity)}
+              onClick={addItem}
               className="p-2 hover:bg-red-100 rounded-full"
             >
               <HiOutlinePlus size={20} className="cursor-pointer" />
@@ -40,7 +44,7 @@ function AddToCartButton({ product, quantity }) {
 
           <button
             onClick={() => addToCart(product, onquantity)}
-            className="bg-[#820C18] text-white text-sm py-2 px-8 sm:px-12 lg:py-3 lg:px-16 rounded-full cursor-pointer hover:bg-red-200 hover:text-[#4C3A2C]"
+            className="bg-[#820C18] text-white text-sm w-38 px-8 py-3 sm:px-12 sm:py-2  lg:px-12 rounded-full cursor-pointer hover:bg-red-200 hover:text-[#4C3A2C]"
           >
             Add to Cart
           </button>
